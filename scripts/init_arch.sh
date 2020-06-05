1c9b00db12b2cb889f5043d20da1291d7b8b7178
Mika Drakolis <mika.drakolis@gmail.com>
2020-06-05 12:43:14 +0200

    Update install scritps

--- scripts/init_arch.sh
+++ scripts/init_arch.sh
@@ -1,1 +1,41 @@
+# Let's begin
+set EDITOR vim
+cd ~
+mkdir install
+
+# Updating the system and getting important utilities
+sudo pacman -Syu
+sudo pacman -S git wget curl base-devel go
+
+# Yay installation
+if hash yay>/dev/null; then
+  echo "yay is executable, let's update"
+  yay
+else
+  echo "Need to install yay first!"
+  cd ~/install
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepg -si
+fi
+
+# Commmon tools installation
+yay -S fish python-pip ffmpeg screenfetch
+
+# Powerline for shell installation
+sudo pip install powerline-shell
+
+# Installing vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Font installation
+yay -S nerd-fonts-hack
+
+# Clean up
+yay -r
+rm ~/install -rf
+
+# Setting up
 git config --global core.excludesfile '~/global.gitignore'
+chsh -s /usr/bin/fish
--- scripts/init_mac.sh
+++ scripts/init_mac.sh
@@ -1,11 +1,35 @@
+# Let's begin
+set EDITOR vim
+
 # Homebrew installation
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if hash brew>/dev/null; then
+  echo "brew is executable, let's update"
+  brew
+else
+  echo "Need to install brew first!"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Updating the system and getting important utilities
+brew -Syu
+brew install git wget go
 
 # Commmon tools installation
-brew install screenfetch
+brew install fish ffmpeg screenfetch
+
+# Powerline for shell installation
+pip3 install powerline-shell
+
+# Installing vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Font installation
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
 
+# Clean up
+
+# Setting up
 git config --global core.excludesfile '~/global.gitignore'
+chsh -s /usr/bin/fish
