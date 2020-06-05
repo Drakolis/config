13de0ca6eb4a5ce5a108c6f8075698611b58dd3a
Mika Zimina <mika.zimina@webfleet.com>
2020-06-05 11:34:43 +0200

    Add some steps into mac install

---
+++ init_mac.sh
@@ -0,0 +1,9 @@
+# Homebrew installation
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Commmon tools installation
+brew install screenfetch
+
+# Font installation
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
