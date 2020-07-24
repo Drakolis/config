b944ac65377a7592308ca37ed806c0e396453b50
Mika Drakolis <mika.drakolis@gmail.com>
2020-07-24 19:15:25 +0200

    Add more modules

--- scripts/polybar/system-bluetooth-bluetoothctl.sh
+++ scripts/polybar/system-bluetooth-bluetoothctl.sh
@@ -3,7 +3,7 @@
 bluetooth_print() {
     bluetoothctl | while read -r; do
         if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-            printf '#1'
+            printf ''
 
             devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
             counter=0
@@ -26,7 +26,7 @@ bluetooth_print() {
 
             printf '\n'
         else
-            echo "#2"
+            echo ""
         fi
     done
 }
